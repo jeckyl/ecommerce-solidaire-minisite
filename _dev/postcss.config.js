9435d238ad6e6ace4a7d2b429fd35801f88a9e08
Yneet <contact@yneet.com>
2020-03-19 15:20:55 +0100

    style form

--- _dev/postcss.config.js
+++ _dev/postcss.config.js
@@ -5,17 +5,19 @@ const whitelister = require('purgecss-whitelister');
 module.exports = {
     plugins: [
         autoprefixer(),
-        purgecss({
-            content: [
-                './../*.html'
-            ],
-            whitelist: [
-                'lazyloaded','fade','show','modal-backdrop'
-                // ...whitelister([
-                //     './assets/sass/common/_syntax.scss',
-                //     './assets/sass/components/_code.scss',
-                // ]),
+        ...process.env.NODE_ENV === 'production' ?
+            purgecss({
+                content: [
+                    './../*.html'
+                ],
+                whitelist: [
+                    'lazyloaded', 'fade', 'show', 'modal-backdrop'
+                    // ...whitelister([
+                    //     './assets/sass/common/_syntax.scss',
+                    //     './assets/sass/components/_code.scss',
+                    // ]),
+                ],
+            })
+        : [],
     ],
-}),
-],
 }
