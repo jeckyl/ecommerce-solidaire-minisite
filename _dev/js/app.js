1e7cb6db260b6bf4c44f7214851e1516851a64b7
Yneet <contact@yneet.com>
2020-03-20 12:13:08 +0100

    add disabled button send

--- _dev/js/app.js
+++ _dev/js/app.js
@@ -38,16 +38,18 @@ $(document).ready(function () {
 
         var form = $(this);
         var url = form.attr('action');
+        var button = $('#submit');
 
         $.ajax({
             type: "POST",
             url: url,
             data: form.serialize(), // serializes the form's elements.
             success: function (msg) {
+                button.prop('disabled', true);
                 if (msg === 'success') {
-                    $('#submit').text('Le formulaire a été envoyé').addClass('btn-success');
+                    button.text('Le formulaire a été envoyé').addClass('btn-success');
                 } else {
-                    $('#submit').text('Une erreur est survenue').addClass('btn-danger');
+                    button.text('Une erreur est survenue').addClass('btn-danger');
                     alert(data); // show response from the php script.
                 }
             }
