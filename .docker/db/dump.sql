de49f115dad53609158528e0c913aabbe32c7bc0
Yneet <contact@yneet.com>
2020-03-24 17:26:07 +0100

    add attribution

--- .docker/db/dump.sql
+++ .docker/db/dump.sql
@@ -44,6 +44,7 @@ CREATE TABLE IF NOT EXISTS `inscriptions` (
   `commentaires` mediumtext NOT NULL,
   `admin` text NOT NULL,
   `slug` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'identifiant boutique',
+  `attribution` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
